ff76a96167f4023e26204c469230818ee2ae379c
Ethan <ethanlramsay@gmail.com>
2018-04-28 22:50:58 +0100

    Added camera functionality
    Image capture, annotation and such

--- twerk.py
+++ twerk.py
@@ -1,6 +1,10 @@
+# MANSEDS Antdroid - Main control file
+# Author: Ethan Ramsay
+
 # Import Dependencies
 import RPi.GPIO as GPIO
 import time
+from picamera import PiCamera
 
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)
@@ -13,10 +17,18 @@ servo = GPIO.PWM(18, 50)
 # Stop function
 def stop(pin):
     GPIO.cleanup()
+    camera.start_preview()
+    time.sleep(2)
+    camera.capture('/image-capture/stopped-'+str(datetime.datetime.now())+'.jpg')
+    camera.stop_preview()  
 
 # Add event detection
 GPIO.add_event_detect(17, GPIO.FALLING, callback=stop)
 
+# Setup camera
+camera = PiCamera()
+camera.resolution(2592, 1944)
+camera.annotate_text = ("Antdroid phone home")
 
 # Twerk
 var = True
@@ -31,4 +43,8 @@ for i in range(50):
 
 servo.start(7)
 time.sleep(0.3)
+camera.start_preview()
+time.sleep(2)
+camera.capture('/image-capture/'+str(datetime.datetime.now())+'.jpg')
+camera.stop_preview()
 GPIO.cleanup()
